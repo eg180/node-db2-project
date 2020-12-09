29d99129a6f1e934f1095b087cd0e23af156023b
Erick Gonzalez <ericksinbox@gmail.com>
2020-12-08 19:32:03 -0500

    api cars endpoint working

--- data/migrations/20201208183624_car-dealers.js
+++ data/migrations/20201208183624_car-dealers.js
@@ -7,7 +7,6 @@ exports.up = function(knex) {
       tbl.text('make', 128).unique().notNullable();
       tbl.text('model', 128).unique().notNullable();
       tbl.text('mileage', 10).unique().notNullable();
-      tbl.text('VIN', 128).unique().notNullable();
       tbl.text('transmission', 128);
       tbl.text('title_status', 128);
 
--- seeds/01-add-vehicles.js
+++ seeds/01-add-vehicles.js
@@ -1,10 +1,10 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('cars').truncate()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
+      return knex('cars').insert([
         {id: 1, VIN: '58694850645628157', make: "Nissan", model: "Xterra", mileage: "122333", transmission: "manual", title_status: "clean"}
       ]);
     });
